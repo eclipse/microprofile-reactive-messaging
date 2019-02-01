5431f0a4be406cd94d38d7a95f3fb651293b94d7
Clement Escoffier <clement.escoffier@gmail.com>
2019-02-01 11:10:25 +0100

    Add supported scope TCK tests
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

---
+++ core-tck/src/main/java/org/eclipse/microprofile/reactive/messaging/tck/scope/ApplicationScopeTest.java
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.eclipse.microprofile.reactive.messaging.tck.scope;
+
+import org.eclipse.microprofile.reactive.messaging.tck.TckBase;
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.Test;
+
+import javax.inject.Inject;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class ApplicationScopeTest extends TckBase {
+
+  @Deployment
+  public static Archive<JavaArchive> deployment() {
+    return getBaseArchive()
+      .addClasses(ApplicationBeans.class);
+  }
+
+  @Inject
+  private ApplicationBeans bean;
+
+  @Test
+  public void verify() {
+    assertThat(bean.getList()).containsExactly(1);
+    assertThat(ApplicationBeans.getStaticList()).containsExactly(1);
+  }
+
+}
---
+++ core-tck/src/main/java/org/eclipse/microprofile/reactive/messaging/tck/scope/DependantBeans.java
@@ -0,0 +1,67 @@
+/*******************************************************************************
+ * Copyright (c) 2018 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * You may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ******************************************************************************/
+package org.eclipse.microprofile.reactive.messaging.tck.scope;
+
+import org.eclipse.microprofile.reactive.messaging.Incoming;
+import org.eclipse.microprofile.reactive.messaging.Outgoing;
+import org.eclipse.microprofile.reactive.streams.operators.ReactiveStreams;
+import org.reactivestreams.Publisher;
+
+import javax.enterprise.context.Dependent;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@Dependent
+public class DependantBeans {
+
+    private static final AtomicInteger COUNTER = new AtomicInteger();
+    private final int id;
+    private List<Integer> list = new ArrayList<>();
+    private static final List<Integer> STATIC_LIST = new ArrayList<>();
+
+    public DependantBeans() {
+        id = COUNTER.getAndIncrement();
+    }
+
+    static List<Integer> getStaticList() {
+        return STATIC_LIST;
+    }
+
+    @Outgoing("source")
+    public Publisher<Integer> source() {
+        return ReactiveStreams.of(id).buildRs();
+    }
+
+    @Incoming("source")
+    @Outgoing("output")
+    public int process(int i) {
+        return i + 1;
+    }
+
+    @Incoming("output")
+    public void sink(int v) {
+        STATIC_LIST.add(v);
+        list.add(v);
+    }
+
+    public List<Integer> getList() {
+        return list;
+    }
+}
