d1b7e587a98cabeb1ec02787c5ad746ed889c8db
Clement Escoffier <clement.escoffier@gmail.com>
2019-05-14 19:49:37 +0200

    Fix license header
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- core-tck/src/main/java/org/eclipse/microprofile/reactive/messaging/tck/signatures/subscribers/SubscriberBean.java
+++ core-tck/src/main/java/org/eclipse/microprofile/reactive/messaging/tck/signatures/subscribers/SubscriberBean.java
@@ -1,15 +1,15 @@
 /**
  * Copyright (c) 2018-2019 Contributors to the Eclipse Foundation
- * <p>
+ *
  * See the NOTICE file(s) distributed with this work for additional
  * information regarding copyright ownership.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * You may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
