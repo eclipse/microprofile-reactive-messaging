e165b8c1b213b2fa0ef20dd14c133120de323a58
Clement Escoffier <clement.escoffier@gmail.com>
2019-05-21 06:53:37 +0200

    Move connector interfaces to the spi package
    Signed-off-by: Clement Escoffier <clement.escoffier@gmail.com>

--- api/src/main/java/org/eclipse/microprofile/reactive/messaging/spi/IncomingConnectorFactory.java
+++ api/src/main/java/org/eclipse/microprofile/reactive/messaging/spi/IncomingConnectorFactory.java
@@ -16,7 +16,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.eclipse.microprofile.reactive.messaging.connector;
+package org.eclipse.microprofile.reactive.messaging.spi;
 
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.reactive.messaging.Message;
